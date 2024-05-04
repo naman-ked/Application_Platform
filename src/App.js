121d2e738ffa5ff2e5c917f412d6745d90199dcf
Kedia <naman_kedia@dell.com>
2024-05-04 23:26:41 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import InfiniteScroll from './Components/InfiniteScroll';
+function App() {
+  return (
+    <div className="App">
+      <InfiniteScroll />
+    </div>
+  );
+}
+
+export default App;
